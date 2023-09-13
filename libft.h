37f8de2ec1f7b414d3af01f95bb48ab25e298b9e
Pere Joan Mateu Salva <pemateu-@car5s3.42barcelona.com>
2023-09-13 15:17:11 +0200

    norminette passes on .h

--- libft.h
+++ libft.h
@@ -6,28 +6,26 @@
 /*   By: pemateu- <pemateu-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:04:49 by pemateu-          #+#    #+#             */
-/*   Updated: 2023/09/13 13:53:44 by pemateu-         ###   ########.fr       */
+/*   Updated: 2023/09/13 15:16:46 by pemateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c);
+int		ft_isalpha(int c);
 
-int	ft_isdigit(int c);
+int		ft_isdigit(int c);
 
-int	ft_isalnum(int c);
+int		ft_isalnum(int c);
 
-int	ft_isprint(int c);
+int		ft_isprint(int c);
 
-int	ft_isascii(int c);
+int		ft_isascii(int c);
 
-int	ft_strlen(const char *c);
+int		ft_strlen(const char *c);
 
-int	ft_toupper(int c);
+int		ft_toupper(int c);
 
-int	ft_tolower(int c);
+int		ft_tolower(int c);
 
 char	*ft_strchr(const char *s, int c);
 
 char	*ft_strrchr(const char *s, int c);
-
-void	ft_putchar(char c);
