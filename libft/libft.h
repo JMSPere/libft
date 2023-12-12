dd88f58f9828570e676d06944310fd8fb414923c
pietrojack <pmateus@uoc.edu>
2023-12-12 18:47:12 +0100

    first commit for ft_itoa.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pemateu- <pemateu-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:04:49 by pemateu-          #+#    #+#             */
-/*   Updated: 2023/10/04 23:12:14 by pemateu-         ###   ########.fr       */
+/*   Updated: 2023/12/12 18:44:28 by pemateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,3 +65,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 
 char	**ft_split(char const *s, char c);
+
+char	*ft_itoa(int n);
